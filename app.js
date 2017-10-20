8baa241331f77eeb4685d4e064d787d75f28f463
Victor Bittencourt <victor.bittencourt@waycarbon.com>
2017-10-20 18:45:04 -0200

    Greeting.txt generator

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log('Hello World');
-console.log('just testing it');
+var fs = require('fs');
+var http = require('http');
+const os = require('os');
+var user = os.userInfo();
+
+console.log('Starting file');
+fs.appendFile('greetings.txt', `Hello, ${user.username}!` , function(err){
+    if(err){
+        console.log('Unable to append string.');
+    }
+    if(!err){
+        console.log('Append successfull');
+    }
+});
